584b76c8b47601ca7d0f7e5d05604f0f8a08817a
Waleed <virtualwaleed@gmail.com>
2021-10-30 18:23:07 +0500

    Backend Auth Design

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = function(req, res, next) {
+module.exports = function(req, res, next ) {
   const token = req.cookies.token
   if (!token) return res.status(401).json({ message: "Auth Error" });
 
   try {
     const decoded = jwt.verify(token, "randomString");
     req.user = decoded.user;
-    next();
+    next()
   } catch (e) {
     console.error(e);
     res.status(500).send({ message: "Invalid Token" });
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,17 +1,99 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth')
+const Task = require('../models/Task')
 
-const {
-    getAllTasks,
-    getTask,
-    createTask,
-    updateTask,
-    deleteTask
-} = require('../controllers/tasks')
+router.get('/', auth, async (req, res) => {
+
+    try {
+        const { user } = req
+        const { id } = user
+        const tasks = await Task.find({ id: user })
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
+})
+
+router.post('/', auth, async (req, res) => {
+    try {
+        const { user } = req
+        const { id } = user
+        const raw = {
+            "user": id,
+            ...req.body
+        }
+        const task = await Task.create(raw)
+        res.status(201).json({ task })
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+})
 
 
-router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.get('/:id', auth, async (req, res) => {
+    const { user } = req
+    if (!user) {
+        return res.status(401).json({ msg: 'Not Logged in' })
+    }
+    try {
+        const { id } = req.params
+        const task = await Task.findOne({ _id: id })
+        if (!task) {
+            return res.status(404).json({ msg: `No task found with id: ${id}` })
+        }
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+
+})
+
+
+router.patch('/:id', auth, async (req, res) => {
+    try {
+        const {user} = req
+        const {id: {userId}} = user
+        const raw = {
+            "user":userId,
+            ...req.body
+        }
+        const {id} = req.params
+        const task = await Task.findOneAndUpdate({_id:id}, raw, {
+            new:true,
+            runValidators:true,
+        })
+
+        if (!task) {
+            return res.status(404).json({msg: `No task found with id: ${id}`})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+})
+
+router.delete('/:id', auth, async (req, res) => {
+    const { user } = req
+    if (!user) {
+        return res.status(401).json({ msg: 'Not Logged in' })
+    }
+    try {
+        const {id} = req.params
+        const task = await Task.findOneAndDelete({_id: id})
+        if(!task) {
+            return res.status(404).json({msg: `No task found with id: ${id}`})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+})
 
 
 module.exports = router
